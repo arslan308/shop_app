075fa97b44f2ee0605be3be6c0be796ab2464c19
Arslan <arslansaleem308@gmail.com>
2020-04-16 15:48:16 +0500

    First commit

---
+++ app/Http/Controllers/ShopifyController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use OhMyBrew\ShopifyApp\Facades\ShopifyApp;
+use Log;
+use DB;
+use App\Insta;
+class ShopifyController extends Controller
+{
+    public function __construct() {
+        $this->middleware(['auth.shop']);   
+    }
+    public function products(Request $request) {
+        return $request;
+    $shop = ShopifyApp::shop();
+    $requests = $shop->api()->rest('GET', '/admin/api/2019-07/products.json');
+    $script_tag = array(
+    "script_tag" => array(
+        "event" => "onload",
+        "src" => "https://ars.taajmart.com/js/custom.js"
+    )
+    );
+    $shop->api()->rest('POST', '/admin/api/2020-04/script_tags.json',$script_tag);
+    $shop_detail = Insta::where('shop_id', '=', $shop->id)->first();
+    return view("welcome", ["products"=>$requests , "shop_detail" => $shop_detail]);
+    }
+    public function insta(Request $request){
+    $shop = ShopifyApp::shop();
+    $user = Insta::where('shop_id', '=', $shop->id)->first();
+   if ($user) {
+    Insta::where('shop_id', '=', $shop->id)->update(['insta_token' => $request->insta_token]);
+   }
+   else{
+    $data=array("shop_id" => $shop->id, "insta_token" =>$request->insta_token);
+    DB::table('instas')->insert($data);
+   }
+    }
+}
